ce2710ab974f0ffb37a9b3fcf22db477c8548b21
Alex Tumwesigye <alex_tumwesigye@ug.jsi.com>
2019-01-17 12:23:06 +0300

    Updating requirements

--- ihisengine/web_app/app/app.py
+++ ihisengine/web_app/app/app.py
@@ -1,9 +1,9 @@
-From flask import flask
+from flask import Flask
 app=Flask(__name__)
 
-app.route("/")
+@app.route("/")
 def index():
     return "Web App"
 
 if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0')
+    app.run(debug=False,host='0.0.0.0:8900')
