b457d1cae25e572c1e94d6085c30db7d6fa09776
Alex Tumwesigye <atumwesigye@gmail.com>
2023-10-15 14:00:50 +0300

    Fix postgres settings

--- dhis2/scripts/set-schedule.sh
+++ dhis2/scripts/set-schedule.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -Eeo pipefail
 # TODO swap to -Eeuo pipefail above (after handling all potentially-unset variables)
-# Set locales
-#export LANG="en_US.UTF-8" 
-#export LC_ALL="en_US.UTF-8" 
-#export LC_CTYPE="en_US.UTF-8"
 
 export CRON_SCHEDULE="${CRON_SCHEDULE:-'0   0   *   *   *'}"
 croncmd="/bin/bash -c '/usr/bin/dhis2-backup-local >> /dbbackups/backups.log 2>&1'"
@@ -18,5 +14,5 @@ EOT
 echo "Added schedule ${CRON_SCHEDULE}";
 echo "Starting crontab"
 /etc/init.d/cron restart
-#echo en_US.UTF-8 UTF-8 >> /etc/locale.gen && locale-gen
+
 /bin/bash -c "/usr/local/bin/docker-entrypoint.sh postgres"
