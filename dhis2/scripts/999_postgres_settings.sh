96ff3681e427c341de26de0c2337d26fb654af55
Alex Tumwesigye <atumwesigye@gmail.com>
2023-10-15 13:42:23 +0300

    Fix postgres script locale

--- dhis2/scripts/999_postgres_settings.sh
+++ dhis2/scripts/999_postgres_settings.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e
+# Perform all actions as $POSTGRES_USER
+export PGUSER="$POSTGRES_USER"
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     alter system set max_connections = 200;
