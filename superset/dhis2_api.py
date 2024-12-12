9525b59f8ecc3f45d24445adb2904b08c6e7853b
Alex Tumwesigye <atumwesigye@gmail.com>
2024-12-12 15:40:51 +0300

    Add filters

--- superset/dhis2_api.py
+++ superset/dhis2_api.py
@@ -263,9 +263,9 @@ class Dhis2ApiEngineSpec(Dhis2ApiParametersMixin,BaseEngineSpec):
         print(f"filter:{analytics_dim}") 
         if analytics_dim is not None:
             response = cls.session.get(f"{ analytics_url }/{ url.get('database','')}/api/analytics/rawData.json?dimension={analytics_dim}&dimension=ou:USER_ORGUNIT&dimension=pe:LAST_12_MONTHS&outputIdScheme=NAME&outputOrgUnitIdScheme=NAME")
-            if response.status_code != 200:
-                raise DatabaseHTTPError(response.text, response.status_code)
-                # Convert to Pandas DataFrame
+            #if response.status_code != 2:
+            #    raise DatabaseHTTPError(response.text, response.status_code)
+            #    # Convert to Pandas DataFrame
                 
             data = response.json()
             
