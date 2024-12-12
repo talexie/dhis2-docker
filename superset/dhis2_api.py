09152d9d8946d82a2e67564a3081d6cd359e10cc
Alex Tumwesigye <atumwesigye@gmail.com>
2024-12-12 16:54:27 +0300

    Add filters

--- superset/dhis2_api.py
+++ superset/dhis2_api.py
@@ -260,14 +260,15 @@ class Dhis2ApiEngineSpec(Dhis2ApiParametersMixin,BaseEngineSpec):
         parsed = sqlglot.parse(sql=query,read="duckdb")
         filters, tables = cls.extract_tables_and_filters(parsed[0])
         analytics_dim = cls.create_analytics_dimension(filters)  
-        print(f"filter:{analytics_dim}") 
+
         if analytics_dim is not None:
-            response = cls.session.get(f"{ analytics_url }/{ url.get('database','')}/api/analytics/rawData.json?dimension={analytics_dim}&dimension=ou:USER_ORGUNIT&dimension=pe:LAST_12_MONTHS&outputIdScheme=NAME&outputOrgUnitIdScheme=NAME")
+            response = cls.session.get(url=f"{ analytics_url }/{ url.get('database','')}/api/analytics/rawData.json?dimension={analytics_dim}&dimension=ou:USER_ORGUNIT&dimension=pe:LAST_12_MONTHS&outputIdScheme=NAME&outputOrgUnitIdScheme=NAME")
             #if response.status_code != 2:
             #    raise DatabaseHTTPError(response.text, response.status_code)
             #    # Convert to Pandas DataFrame
                 
             data = response.json()
+            print(f"data:{ data }")
             
             df = pd.dataFrame(cls.format_analytics_data(data))
         
