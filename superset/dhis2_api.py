8b46224a4ad875d9e235f81fa70125ea5c52d540
Alex Tumwesigye <atumwesigye@gmail.com>
2024-12-17 21:07:37 +0300

    Add filters

--- superset/dhis2_api.py
+++ superset/dhis2_api.py
@@ -43,6 +43,7 @@ from superset.views.base import BaseFilter
 from superset.models.helpers import ExploreMixin
 from superset.models.dashboard import Dashboard
 from superset.models.slice import Slice
+from superset import db
 
 import duckdb, requests, sqlglot
 from requests import Session
@@ -302,13 +303,21 @@ class Dhis2ApiEngineSpec(Dhis2ApiParametersMixin,BaseEngineSpec,ExploreMixin):
         #print("FIL:",get_dataset_access_filters(database))
         #print(database.data)
         #print('6:',cls.get_query_str_extended(query))
-        print('6:',request.form_data)
+        print('6:',request.form.to_dict())
         ob = Dashboard()
         print("X1:",ob.data)
         print("X2:",ob.charts)
         sl = Slice()
         print("X3:",sl.form_data)
-        
+        form_data = {}
+        if slice_id := request.args.get("slice_id"):
+            slc = db.session.query(Slice).filter_by(id=slice_id).one_or_none()
+            print("slc:",slc)
+            if slc:
+                form_data = slc.form_data.copy()
+
+        print("form_data:",form_data)
+        print("Args;",request.args)
         if analytics_dim is not None and 'analytics' in tables:
             url_endpoint = f"https://{ analytics_url }/{ url.get('database','')}/api/analytics/rawData.json?dimension={analytics_dim}&dimension=ou:USER_ORGUNIT&dimension=pe:LAST_12_MONTHS&outputIdScheme=NAME&outputOrgUnitIdScheme=NAME"
             print(url_endpoint)
